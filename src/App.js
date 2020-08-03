0ffaf8173637f449f54306cfd777215197217655
Mickey Nash <michaelpeternash@gmail.com>
2020-08-03 17:17:53 -0400

    Sketched out basics.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import SWAPIService from './swapi-service';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.swapiService = new SWAPIService();
+    this.state = {
+      items: null
+    }
+  }
+
+  componentDidMount(){
+    this.getItems();
+  }
+  
+  render(){
+    const items = this.state.items;
+    if(!items) return null;
+    const listItems = items.map((item) =>
+      <li>
+        <span className="item-name">{item.name}</span>
+      </li>
+    )
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>This is a header.</h1>
+        </header>
+        <ol>
+          {listItems}
+        </ol>
+        <p>halp</p>
+      </div>
+    );
+  }
+
+  getItems(){
+    this.swapiService.retrieveItems()
+    .then(items => {
+      this.setState({items: items});
+    });
+  }
 }
 
 export default App;
